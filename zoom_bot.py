a8ad5ed4183e44775fcc4e537f1dcc656952ef93
rawheel <rawheeelalisiddiqui@gmail.com>
2020-10-19 19:08:53 +0500

    zoom bot version 0.1

---
+++ zoom_bot.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import datetime
+import time
+import keyboard
+
+class zoom_bot:
+	def join(self,meeting_link):
+		self.bot = webdriver.Chrome("chromedriver.exe")
+		self.bot.get(meeting_link)
+		time.sleep(3)
+		keyboard.send("tab", do_press=True, do_release=True)
+		keyboard.send("tab", do_press=True, do_release=True)
+		keyboard.send("enter", do_press=True, do_release=True)
+		time.sleep(10)
+
+		self.bot.quit()
+
+link = open("meeting_link.txt","r").read()
+
+obj = zoom_bot()
+obj.join(link)
+
